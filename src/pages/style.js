6c2af46124dc6c6f4d0b5e045fdcc6c38d1bf74d
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-09-01 14:27:31 +0500

    create separate file for some tailwind classes

---
+++ src/pages/style.js
@@ -0,0 +1,4 @@
+export const changeViewBtn = 'justify-self-center bg-green-500 text-white m-2 px-3 py-2 rounded hover:bg-green-400 active:bg-green-800 focus:outline-none focus:ring focus:ring-green-500';
+export const sortByBtn = 'text-white m-2 px-3 py-2 rounded hover:bg-green-500 active:bg-green-800 focus:outline-none focus:ring';
+export const prevNextBtn = 'text-white m-2 px-3 py-2 rounded bg-green-500 hover:bg-green-400 active:bg-green-800 focus:outline-none focus:ring focus:ring-green-500';
+export const paginationBtn = 'text-white m-1 px-2 py-1 rounded bg-green-500 hover:bg-green-400 active:bg-green-800 focus:outline-none focus:ring focus:ring-green-500';
